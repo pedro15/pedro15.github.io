a31127f9a6a856c009f1f2ee98199750780ce3d0
Pedro Duran <pedrojosuedm@hotmail.com>
2018-03-24 19:33:05 -0400

    finalizada estructura

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+      }
+    }
+  });
+
+  var elm = $('.card-img').children('img');
+  elm.mouseenter(function(event){
+    $(event.currentTarget).transition({scale:1.2},300);
+  });
+
+  elm.mouseleave(function(event){
+    $(event.currentTarget).transition({scale:1.0},300);
+  });
